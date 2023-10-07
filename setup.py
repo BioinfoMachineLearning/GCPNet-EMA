9d65bb88983657836f89205e9015eabbcaa881f9
Alex Morehead <alex.morehead@gmail.com>
2023-10-07 15:45:35 -0500

    Add new environment.yal

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="GCPNet-EMA",
     version="0.0.1",
-    description="Describe Your Cool Project",
-    author="",
-    author_email="",
-    url="https://github.com/user/project",
+    description="Source code for the paper 'Protein Structure Accuracy Estimation using Geometry-Complete Perceptron Networks'.",
+    author="Alex Morehead",
+    author_email="acmwhb@umsystem.edu",
+    url="https://github.com/BioinfoMachineLearning/GCPNet-EMA",
     install_requires=["lightning", "hydra-core"],
     packages=find_packages(),
     # use this to customize global commands available in the terminal after installing the package
